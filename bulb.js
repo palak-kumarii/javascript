7645a94e3a2bce9c374a218058096f0ff9c12883
palak-kumarii <pkumari.1712005@gmail.com>
2025-07-03 12:14:02 +0530

    add

---
+++ bulb.js
@@ -0,0 +1,21 @@
+const bulbOnURL = 'https://res.cloudinary.com/dcgghyuav/image/upload/v1751522915/bulb-on_va0euz.jpg';
+const bulbOffURL = 'https://res.cloudinary.com/dcgghyuav/image/upload/v1751453496/bulb-off_rgjumy.jpg';
+const catOnURL = 'https://res.cloudinary.com/dcgghyuav/image/upload/v1751522902/cat-on_is0so2.jpg';
+const catOffURL = 'https://res.cloudinary.com/dcgghyuav/image/upload/v1751453489/cat-off_pz4bri.jpg';
+
+
+const bulbImg = document.getElementById('bulb');
+const catImg = document.getElementById('cat');
+const onBtn = document.getElementById('onbtn');
+const offBtn = document.getElementById('offbtn');
+
+
+onBtn.addEventListener('click', () => {
+    bulbImg.src = bulbOnURL;
+    catImg.src = catOnURL;
+});
+
+offBtn.addEventListener('click', () => {
+    bulbImg.src = bulbOffURL;
+    catImg.src = catOffURL;
+});
--- count.js
+++ count.js
@@ -1,42 +1,19 @@
-// Initial count value
 let count = 0;
 
-// Get DOM elements
-const countEl = document.getElementById('count');
-const incBtn = document.getElementById('increase');
-const decBtn = document.getElementById('decrease');
-const resetBtn = document.getElementById('reset');
-
-// Updates the displayed number and its color
-function updateDisplay() {
-    countEl.textContent = count;
-    if (count > 0) {
-        countEl.style.color = 'green';
-    } else if (count < 0) {
-        countEl.style.color = 'red';
-    } else {
-        countEl.style.color = 'black';
-    }
-
+function increase() {
+    count++;
+    document.getElementById("count").innerText = count;
+    document.getElementById("count").style.color = "darkgreen";
 }
 
-// Event handlers
-incBtn.addEventListener('click', () => {
-    count++;
-    updateDisplay();
-});
-
-decBtn.addEventListener('click', () => {
+function decrease() {
     count--;
-    updateDisplay();
-});
+    document.getElementById("count").innerText = count;
+    document.getElementById("count").style.color = "red";
+}
 
-resetBtn.addEventListener('click', () => {
-    if (count !== 0) {
-        count = 0;
-        updateDisplay();
-    }
-});
-
-// Initialize when loaded
-updateDisplay();
+function reset() {
+    count = 0;
+    document.getElementById("count").innerText = count;
+    document.getElementById("count").style.color = "black";
+}
